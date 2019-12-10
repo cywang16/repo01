73d49500a0411b2bf4d8b90bd2131d1250c7a97a
Chun-Yen Wang <cywang16@gmail.com>
2019-12-10 13:26:44 -0800

    update projects

---
+++ Swift/Concentration/Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Chun-Yen Wang on 10/17/19.
+//  Copyright © 2019 Chun-Yen Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- Swift/MySwiftFirst/MySwiftFirst/ViewController.swift
+++ Swift/MySwiftFirst/MySwiftFirst/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("Record button is pressed")
+        recordingLabel.text = "Recording in progress"
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        print("Stop Recording button is pressed")
+        recordingLabel.text = "Tab to Record"
+    }
 }
 
